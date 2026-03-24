b1e9697f53332f5d36142ccbae9a5b45f57dce07
Morning-Hu <168148044+Morning-Hu@users.noreply.github.com>
2026-03-24 11:07:08 +0800

    Update output messages in main.cpp to English

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,25 @@ int main()
 
         int ans = KnapSolver::knapSack(cap, items);
 
-        cout << "背包容量：" << cap << endl;
-        cout << "物品数量：" << items.size() << endl;
-        cout << "最大价值：" << ans << endl;
+        cout << "bag cap#include <iostream>
+#include "DataLoader.h"
+#include "KnapSolver.h"
+
+using namespace std;
+
+int main()
+{
+    try
+    {
+        auto data = DataLoader::loadData("data.txt");
+        int cap = data.first;
+        auto items = data.second;
+
+        int ans = KnapSolver::knapSack(cap, items);
+
+        cout << "Bag Capacity:" << cap << endl;
+        cout << "Number of Items:" << items.size() << endl;
+        cout << "Max Value:" << ans << endl;
     }
     catch (exception& e)
     {
